a6a88d8cd1dd9d08845b519fe7864a3cd17670af
Vanessa Teo <vtteo@student.unimelb.edu.au>
2025-09-09 14:11:59 +1000

    add layout bg colour and dashboard header

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
+
+
+
+export default function DashboardPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <DashboardHeader /> 
+     
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{background: "#ffd9b3"}}
       >
         {children}
       </body>
