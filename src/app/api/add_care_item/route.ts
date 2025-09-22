4cef471ad2b09ff1ec43a8f53ae9b97bd996d40a
Zahra Rizqita <zahra.rizqita15@gmail.com>
2025-09-22 22:15:10 +1000

    add API route and added categories to schema

---
+++ src/app/api/add_care_item/route.ts
@@ -0,0 +1,17 @@
+import {NextResponse, NextRequest } from "next/server";
+import {connectDB} from "@/lib/mongodb";
+import CareItem from "@/model/CareItem"
+
+export async function POST(req: NextRequest) {
+    await connectDB();
+
+    try{
+        const body = await req.json();
+        const item = await CareItem.create(body);
+
+        return NextResponse.json(item,{status: 201});
+    } catch(err) {
+        console.error(err);
+        return NextResponse.json({error: "Server error"}, {status: 500});
+    }
+}
--- src/model/CareItem.ts
+++ src/model/CareItem.ts
@@ -4,11 +4,13 @@ import {Schema, model, models} from "mongoose";
 const careItemSchema = new Schema (
     {
         name: { type: String, required: true},
-        frequency: { type: String, enum: ["once, weekly, monthly"], default: "once"},
+        frequency: { type: String, enum: ["once", "weekly", "monthly"], default: "once"},
         startDate: { type: Date},
         repeatYearly: {type: Boolean, default: false},
+        category: { type: String, enum: ["hygiene", "clothing", "medical", "food", "support", "household", "other"], default: "other"},
         status: { type: String, enum: ["pending", "done"], default: "pending"},
-    }
+    },
+    { timestamps: true }
 );
 
 const CareItem = models.CareItem || model("CareItem", careItemSchema);
