54d34adfadcea26e2e10d2c30dd206cb9506c0fb
Zahra Rizqita <zahra.rizqita15@gmail.com>
2025-09-23 13:31:35 +1000

    Added server connection

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if(!MONGODB_URI) throw new Error("Please add uri into .env.local");
+
+declare global {
+    var _mongooseConnection: {isConnected?: boolean} | undefined;
+}
+
+export const connectDB = async () => {
+    if(global._mongooseConnection?.isConnected) return mongoose.connection;
+
+    try {
+        await mongoose.connect(MONGODB_URI);
+        global._mongooseConnection = {isConnected: true};
+        console.log("MongoDB is connected via Mongoose");
+        return mongoose.connection;
+    } catch(err) {
+        console.error("MongoDB connection error:", err);
+        throw err;
+    }
+};
